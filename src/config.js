1fed8c293b6a0f1fd8b5cb3310c6abb96e633851
Hydeo <p.garciapelayo@gmail.com>
2018-09-20 19:12:52 +0200

    Cors again

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const conf_dev = {
 	//https://mbc-mongo-back.herokuapp.com/
-	url_api : "http://localhost:8080",
+	url_api : "https://mbc-mongo-back.herokuapp.com",
 	breakpoints : {
 		S : 600, 
 		M : 900, 
