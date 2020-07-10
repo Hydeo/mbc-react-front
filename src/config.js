d5ccac8c96ddab6632a9104ea0f99515c79f47c1
Hydeo <p.garciapelayo.com>
2020-07-10 17:57:54 +0100

    revert ignore config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const conf_dev = {
+	//https://mbc-mongo-back.herokuapp.com/
+	//url_api : "http://localhost:8080/",
+	url_api : "https://mbc-mongo-back.herokuapp.com",
+	breakpoints : {
+		S : 600, 
+		M : 960, 
+		L : 1280
+	},
+
+	isotope_nb_item : {
+		S : 2,
+		M : 3,
+		L : 5
+	}
+}
